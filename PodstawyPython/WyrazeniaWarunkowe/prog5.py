a3ddf3b68d747b064a06a9118b7a2e4db7adca16
lamufinek <lamufinek@gmail.com>
2019-11-26 07:18:57 +0100

    lamufinek

---
+++ PodstawyPython/WyrazeniaWarunkowe/prog5.py
@@ -0,0 +1,12 @@
+#Programy przetwarzane (realizowane) są sekwencyjnie- tzm. linijka po linijce
+#Program napisany przez programistę w języku python jest tłumaczony na kod binarny (zera i jedynki)
+#Ciąg zer i jedynek jest możliwy do przetworzenia przez procesor i może być przechowywany w pamięci RAM (może mieć fizyczną reprezentację w pamięci RAM)
+#Ciąg zer i jedynek (tzn. zapis binarny) "jest językiem procesora"
+#Python jako język jest językiem programisty i w swojej formie jest zbliżony do języka jakim posługujemy się nacodzień (tj. języka naturalnego)
+
+'''Jeżeli chcemy by poszczególne polecenia/fragmenty kodu były wykonane
+zależnie od zaistnienia jakichś warunków (spełnienia jakichś warunków) powinniśmy
+wykorzystać wyrażenia warukowe'''
+
+if(567>467):            #jeżeli wyrażenie zawarte w nawisie będzie prawdziwe
+    print("zabawa")     #P
