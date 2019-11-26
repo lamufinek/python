c41b3bb70e66c8237ab0a84ec46b9fddd2706fba
lamufinek <lamufinek@gmail.com>
2019-11-26 08:51:00 +0100

    lamufinek

--- PodstawyPython/WyrazeniaWarunkowe/INSTRUKCJE_WARUNKOWE.py
+++ PodstawyPython/WyrazeniaWarunkowe/INSTRUKCJE_WARUNKOWE.py
@@ -20,6 +20,10 @@ if(567>467):            #jeżeli wyrażenie zawarte w nawisie będzie prawdziwe
 if(467>567):            #jeżeli wyrażenie zawarte w nawisie NIE będzie prawdziwe
     print("dygnitarz")  #Polecenie po dwukropku nie zostanie zrealizowane
 
+
+###PRZERWA NA HERBATKĘ, SOCZEK, SIKU
+
+
 #pamiętaj, że w języku Python to wcięcia ze znaków białych (spacji i tabulatorów)
 #decydują o tym czy któryś fragment twojego programu jest zawarty w danej strukturze logicznej (if, while, for itp)
 #czy jednak nie jest w niej zawarty. I tak poniżej:
@@ -37,6 +41,11 @@ if(67>340): #czyli wyrażenie w nawisie nie jest prawdziwe, bo czy 67 faktycznie
     print("skonał")     #A warunek w nawisie nie jest spełniony
 print("BOOM")           #dopiero słowo BOOM nie jest elementem wyrażenia warunkowego
 
+#Poeksperymentuj
+
+###PRZERWA NA HERBATKĘ, SOCZEK, SIKU
+
+
 
 '''I na koniec dla dociekliwych- albo w sumie to dla wszystkich :P :
 Wbrew temu co sobie często myślimy o komputerach, że to jakieś czary,
@@ -47,8 +56,40 @@ współczesne procesory składają się z wielu milionów elementów elektronicz
 '''Między innymi dzięki działowi matematyki jakim jest LOGIKA, w 20. wieku naukowcy stworzyli
 z elementów elektronicznych struktury logiczne- przykładem jest właśnie współczesny procesor. I tu dochodzimy do sedna:
 język binarny, czyli zerojedynkowy, czyli zera i jedynki, czyli ciąg np: 010001010100101010010100101010010101,
-czyli wyrażenie w systemie dwójkowym (ponieważ opiera się na 2 cyfrach, w opozycji do naszego systemu liczbowego dziesiętnego, bo mamy 10 palców u rąk i nóg)
+czyli wyrażenie w systemie dwójkowym (ponieważ opiera się na 2 cyfrach,
+w opozycji do naszego systemu liczbowego dziesiętnego, bo mamy 10 palców u rąk i nóg)
+I tak: skoro nasz procesor to układ elektroniczny, który przetwarza ciąg zer i jedynek:
+TUTAJ PAMIĘTAJ- liczby i matematyka są tylko abstrakcją stworzoną przez człowieka by opisać, usystematyzować różne zjawiska!!! 
+To "nasze/ludzkie logiczne 0" będzie oznaczało brak napięcia elektrycznego(tego samego napięcia elektrycznego, które kryje się w bateriach :P )
+To "nasze/ludzkie logiczne 1" będzie oznaczało obecność napięcia.
 
+Głowa boli, co?
 '''
-if("żaba"):
-    print("miarka się przebrała!")
+#I dla filozofów: Fizyka+Elektronika+Matematyka+Lingwistyka(nauka o językach)= Współczesny Język Programowania
+
+###PRZERWA NA HERBATKĘ, SOCZEK, SIKU
+"""LOGIKA (ten dział matematyki :P ) mówi, że:
+NIE Prawda = Fałsz
+NIE Fałsz = Prawda
+I tak w nawiasie wyrażenia warunkowego możemy zawrzeć nie tylko złożone wyrażenie:
+np 45>78, 45+67+89>67, czy 39/3>13*2 ("/" to znak dzielenia, "*" to znak mnożenia)
+ale również samo:
+0 czyli logiczny fałsz
+i każdą inną liczbę, a każda inna liczba to nie 0 czyli nie fałsz, czyli PRAWDA
+"""
+if(0):  #czyli wewnątrz nawiasu nie fałsz=warunek niespełniony 
+    print("grzesznik") #nie zostanie wyświetlony
+
+if(789):    #czyli nie zero, czyli NIE FAŁSZ, czyli PRAWDA
+    print("rewolwer")
+    print("BOOM")
+    print("BOOM")   #rewolwer, bum, bum, bum zostanie wyświetlone
+    print("BOOM")
+    print("BOOM")
+
+
+if(789+678):    #a nawet można dodawać: nie zero plus nie zero, to NIE FAŁSZ+NIE FAŁSZ=PRAWDA+PRAWDA=PRAWDA 
+    print("maniok") #to taka roślina uprawna, a jej nazwa zostanie wyświetlona
+
+
+### DOŚĆ na DZIŚ
