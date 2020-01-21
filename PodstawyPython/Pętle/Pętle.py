263c1e493102b7d02ac8ec451045eaf4e331a89a
lamufinek <lamufinek@gmail.com>
2020-01-21 11:07:23 +0100

    Create Pętle.py

---
+++ PodstawyPython/Pętle/Pętle.py
@@ -0,0 +1,23 @@
+#Przechodzimy do sedna- postaramy się zrozumieć czym jest język programowania
+#Skoro wszystkie informacje przetwarzane przez procesor zapisane są za pomocą 0 i 1
+#Skoro wszystkie dane są przechowywane na dysku w postaci 0 i 1
+#Pamiętajmy, że są to 0 i 1 w naszym, ludzkim (teoretycznym) mniemaniu
+#W praktyce procesor przetwarza sygnały elektryczne-
+#albo ludzkie 0 czyli brak sygnału, albo ludzkie 1 czyli obecność sygnału
+#Tak samo pamięci komputerowe teoretycznie przechowują 0 i 1, a w prkatyce są to
+#Ładunki elektryczne przechowywane w poszczególnych elementach elektronicznych:
+#0 oznacza brak ładunku, a 1 oznacza obecność ładunku elektrycznego
+
+
+#PONIŻEJ SEKCJA DLA CIEKWASKICH:
+
+#w pamięci RAM mamy ładunek elektryczny przechowywany przez kondensatory- informacje zanikają po odcięciu zasilania
+#w pamięci podręcznej procesora używane są przerzutniki (złożone z wielu tranzystorów)-informacje zanikają po odcięciu zasilania
+#na dysku twardym typu HDD 0 i 1 przechowywane są w formie ładunków magnetycznych(1) lub ich braku(0)-informacje są przhowywane po zaniku zasilania
+#na dysku twardym typu SSD 0 i 1 przechowujemy wewnątrz tranzystorowej bramki pamiętającej-informacje są przhowywane po zaniku zasilania
+
+
+
+#KONIEC SEKCJI DLA CIEKAWSKICH
+
+
