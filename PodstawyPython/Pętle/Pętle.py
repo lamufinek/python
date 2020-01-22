ce82c6352f9765b5d4b6cc9b17bceff87567742a
lamufinek <lamufinek@gmail.com>
2020-01-22 10:45:45 +0100

    Update Pętle.py

--- PodstawyPython/Pętle/Pętle.py
+++ PodstawyPython/Pętle/Pętle.py
@@ -3,7 +3,7 @@
 #Skoro wszystkie dane są przechowywane na dysku w postaci 0 i 1
 #Pamiętajmy, że są to 0 i 1 w naszym, ludzkim (teoretycznym) mniemaniu
 #W praktyce procesor przetwarza sygnały elektryczne-
-#albo ludzkie 0 czyli brak sygnału, albo ludzkie 1 czyli obecność sygnału
+#albo ludzkie 0 czyli brak sygnału, albo ludzkie 1 czyli obecność sygnału eletrycznego
 #Tak samo pamięci komputerowe teoretycznie przechowują 0 i 1, a w prkatyce są to
 #Ładunki elektryczne przechowywane w poszczególnych elementach elektronicznych:
 #0 oznacza brak ładunku, a 1 oznacza obecność ładunku elektrycznego
@@ -19,5 +19,14 @@
 
 
 #KONIEC SEKCJI DLA CIEKAWSKICH
+
+#Skoro procesor (który jest "sercem" układu komputerowego) przetwarza dane zapisane w postaci sygnałów elektrycznych
+#Skoro procesor został zaprojektowany w taki sposób (nie istnieją na razie inne procesory), aby wykonywać operacje logiczne i algebraiczne (głównie dodawanie)
+#na sygnałach elektrycznych to należy zastanowić się w jaki sposób programowano pierwsze komputery
+#pomocny okaże się film (https://www.youtube.com/watch?v=nwDq4adJwzM)- polecam od czasu 5.20
+#Co widzimy na filmie? Widzimy, że na wczesnym etapie rozwoju procesor otrzymywał polecenia zapisane
+#w postaci dziurkowanych kart (dziurki na kartach znajdowały się w odpowiednich rzędach i w odpowiednich kolumnach i symbolizowały jakżeby inaczej 0 i 1!)
+#dziurki na kartach były przetwarzane przez specjalne urządzenie na ciąg sygnałów elektrycznych dla procesora
+#procesor wykonywał operacje (kiedyś były to najczęście obliczenia) i zwracał wynik (drukował go również na karcie lub wyświetlał na wyświetlaczu o ile go posiadał)
 
 
