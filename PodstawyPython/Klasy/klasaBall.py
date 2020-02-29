6a51bd66e9e74202615fc601d69318096e08b65e
lamufinek <lamufinek@gmail.com>
2020-02-29 14:03:32 +0100

    Update klasaBall.py

--- PodstawyPython/Klasy/klasaBall.py
+++ PodstawyPython/Klasy/klasaBall.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import time
 import keyboard
+import random
 
 okienko = Tk()
 kanwas = Canvas(okienko, width = 400, height =400)
@@ -12,21 +13,23 @@ kanwas.pack()
 class Ball():
     def __init__(self,kanwas, promien, pozycjaX, pozycjaY, kolor):
         self.id = kanwas.create_oval(pozycjaX,pozycjaY,pozycjaX+promien,pozycjaY+promien, fill = kolor)
-        self.pozycjaX = pozycjaX
-        self.pozycjaY = pozycjaY
+        self.pozycjaX = pozycjaX+promien/2
+        self.pozycjaY = pozycjaY+promien/2
         self.kanwas = kanwas
 
     def motionUp(self):
         self.kanwas.move(self.id,0,-1)
+        self.pozycjaY= self.pozycjaY - 1
 
     def motionDown(self):
         self.kanwas.move(self.id,0,1)
-
+        self.pozycjaY= self.pozycjaY + 1
     def motionRight(self):
         self.kanwas.move(self.id,1,0)
-
+        self.pozycjaX= self.pozycjaX + 1
     def motionLeft(self):
         self.kanwas.move(self.id,-1,0)
+        self.pozycjaX= self.pozycjaX - 1
 
     def Control(self):
         if(keyboard.is_pressed("Up")):
@@ -38,18 +41,32 @@ class Ball():
         if(keyboard.is_pressed("Left")):
             self.motionLeft()
 
+class Item():
+    def __init__(self, kanwas, promien, pozycjaX, pozycjaY, kolor):
+        self.id = Ball(kanwas, promien, pozycjaX+promien, pozycjaY+promien, kolor)
+        self.pozycjaX = pozycjaX
+        self.pozycjaY = pozycjaY
+        self.canvas = kanwas
+
+    def distanceControll(self,pozycjaX,pozycjaY):
+        if(abs(self.pozycjaX-pozycjaX)<=20 and abs(self.pozycjaY-pozycjaY)<=20 ):
+            self.canvas.delete(self.id.id)
+
 
 
 
 kulka1 = Ball(kanwas, 40 , 50, 50, "red")
-kulka2 = Ball(kanwas, 70, 100, 100, "green")
-kulka3 = Ball(kanwas, 100, 150, 150, "pink")
+tablicaItem = []
+for i in range(0,56):
+    item = Item(kanwas, 5, random.randint(20,380), random.randint(20,380), "pink")
+    tablicaItem.append(item)
 
 
 while(True):
     time.sleep(0.01)
     kulka1.Control()
-    kulka2.Control()
+    for i in tablicaItem:
+        i.distanceControll(kulka1.pozycjaX, kulka1.pozycjaY)
     okienko.update()
     if(keyboard.is_pressed("q")):
         break
