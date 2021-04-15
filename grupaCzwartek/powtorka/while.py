d7ebe36af3fbb9ad3917fed52a94b117d0ab14da
lamufinek <lamufinek@gmail.com>
2021-04-15 19:44:05 +0200

    while

---
+++ grupaCzwartek/powtorka/while.py
@@ -0,0 +1,29 @@
+#wyprintuj wszystkie liczby od 100 do 0 w petli while:
+zmienna = 100
+
+while(zmienna>0):
+    print(zmienna)
+    zmienna = zmienna - 1
+
+
+#wyprintuj wszystkie liczby od 100 do 0 w petli while:
+#sposob 2
+
+
+while not(zmienna == 0):
+    print(zmienna)
+    zmienna = zmienna - 1
+
+#wyprintuj wszystkie liczby od 100 do 0 w petli while:
+#sposob 3
+#while(zmienna >= 0):
+while(zmienna != 0):
+    print(zmienna)
+    zmienna = zmienna - 1
+
+a==b #a równe b
+a>b #a większe od b
+a<b #a mniejsze od b
+a>=b   # a większe lub równe b
+a<=b
+a!=b    #a różne od b
