b9217e5eab7348680c956576ccd86c066ea4bbe0
lamufinek <lamufinek@gmail.com>
2021-04-16 17:48:47 +0200

    Create randomPrzyklad.py

---
+++ grupaPiatek2/randomPrzyklad.py
@@ -0,0 +1,13 @@
+######jeśli importujesz tak
+from random import *
+
+####funkcję wywołujesz tak:
+zmiennaA  = randint(20,40)
+
+
+
+######jeśli importujesz tak
+import random
+
+####funkcję wywołujesz tak:
+zmiennaB  = random.randint(20,40)
