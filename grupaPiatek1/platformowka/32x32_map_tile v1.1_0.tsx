ca9820a7f0017f92f790100eee8b4b63f51bd43a
lamufinek <lamufinek@gmail.com>
2021-06-18 17:13:12 +0200

    dupa

---
+++ grupaPiatek1/platformowka/32x32_map_tile v1.1_0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="32x32_map_tile v1.1_0" tilewidth="32" tileheight="32" tilecount="170" columns="17">
+ <image source="32x32_map_tile v1.1_0.png" width="568" height="329"/>
+</tileset>
