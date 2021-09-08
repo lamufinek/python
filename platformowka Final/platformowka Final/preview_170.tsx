514b6fa49ca3647fddb1b18e6d2061419b1537c1
lamufinek <lamufinek@gmail.com>
2021-09-08 21:23:55 +0200

    d

---
+++ platformowka Final/platformowka Final/preview_170.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="preview_170" tilewidth="32" tileheight="32" tilecount="696" columns="29">
+ <image source="preview_170.png" width="954" height="780"/>
+</tileset>
