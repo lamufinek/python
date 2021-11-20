300783fa58a675b90f20eef04c83152f929f5257
lamufinek <lamufinek@gmail.com>
2021-11-20 11:39:47 +0100

    Update Sortowanie.py

--- PiatekPython/Sortowanie.py
+++ PiatekPython/Sortowanie.py
@@ -1,22 +1,53 @@
+
 from random import *
+from turtle import *
 
-tablicaLiczb = []
 
-for y in range(0,100):
-    a = randint(0,10000)
-    tablicaLiczb.append(a)
+
+pozycje = []
+
+for i in range(-250,250,10):
+    pozycje.append(i)
+
+
+
+class Circle():
+    def __init__(self, size, x, y):
+        self.size = size
+        self.x = x
+        self.y = y
+        self.body = Turtle()
+        self.body.shape("circle")
+        self.body.penup()
+        self.body.speed(1000)
+
+        self.body.goto(x,y)
+        self.body.shapesize(size*3,size)
+        self.body.speed(1)
+
+kulki = []
+for position in pozycje:
+    size = uniform(0.1, 0.5)
+    obiekt = Circle(size, position, 0)
+    kulki.append(obiekt)
 
 
 i= 0 
-while(i<len(tablicaLiczb)-1):
-    if(tablicaLiczb[i]>tablicaLiczb[i+1]):
-        temp = tablicaLiczb[i]
-        tablicaLiczb[i] = tablicaLiczb[i+1]
-        tablicaLiczb[i+1] = temp
-        i = 0
+while(i<len(kulki)-1):
+    if(kulki[i].size>kulki[i+1].size):
+        temp = kulki[i].body.xcor()
+        kulki[i].body.goto(kulki[i+1].body.xcor(),0) 
+        kulki[i+1].body.goto(temp,0)
+        newKulki = []
+        temp = kulki[i]
+        kulki[i]=kulki[i+1]
+        kulki[i+1]=temp
+    
+        i = -1
     i = i+1
+input()
 
 
 
 
-print(tablicaLiczb)
+
